6b57c6bdf74393ebb74f5089cbd5e2f6e7ba3f8d
Saurabh Asthana <faplap@gmail.com>
2020-07-28 20:36:41 -0700

    some refactoring

--- dscobal.js
+++ dscobal.js
@@ -1,14 +1,11 @@
 let {cos,sin,PI,random,floor,min,max} = Math;
 
-let createElement = (type, atts) => {
+const createElement = (type, atts) => {
   let shape = document.createElementNS("http://www.w3.org/2000/svg", type); 
   // Set any attributes as desired
   Object.keys(atts).forEach(att => shape.setAttribute(att, atts[att]));
   return shape;
 }
-
-let txt = (id) => document.querySelector('#' + id).value;
-let val = (id) => parseFloat(txt(id));
 
 let stroke = 'darkgoldenrod'
 let stroke_width = 6;
@@ -16,73 +13,71 @@ let stroke_width = 6;
 let path = (fill, stroke, stroke_width, ...points) => createElement('path',
   { fill, stroke, 'stroke-width': `${stroke_width}px`, d: `M ${points.map(([x,y])=>`${x} ${y}`).join(' ')} Z` }
 );
-let times = (n) => (iterator) => Array(n).fill().map((_,i) => iterator(i));
-let radians = (deg) => PI * (deg / 180);
+
+const times = (n) => (iterator) => Array(n).fill().map((_,i) => iterator(i));
+const radians = (deg) => PI * (deg / 180);
 
 let colorMatrix = [];
 
-let r = 300;
-let cx = 400, cy = 400;
+const getColor = (i,j) => colorMatrix[i] ? colorMatrix[i][j] : 'red';
 
-let color = (i,j) => colorMatrix[i] ? colorMatrix[i][j] : 'red';
+const makeColorMatrix = () => {
+  let {color, lat_lines, lon_lines, rrate} = readConfig();
 
-let lat_lines = 16;
-let lon_lines = 16;
-
-let replacement_rate = 0.3;
-
-let makeColorMatrix = () => {
-  let colors = txt('color').split(/\s+/);
+  colors = color.split(/\s+/);
   colorMatrix = times(lat_lines)( i => times(lon_lines)( j => 
-        random() < replacement_rate || !colorMatrix.length
-        ?  colors[floor(random() * colors.length)]
-        : color(i,j)
+        random() < rrate || !colorMatrix.length
+        ? colors[floor(random() * colors.length)]
+        : getColor(i,j)
   ))
 }
-window.onload = () => makeColorMatrix();
 
-let io = 0;
-let inc = (i) => (PI * i / lat_lines) % PI;
-let azi = (j) => (io + 2 * PI * j / lon_lines) % (2 * PI);
+const readConfig = () => {
+  const num = [ 'wvel', 'wcx', 'wcy', 'wspokes', 'vel', 'lat_lines',
+    'lon_lines', 'stroke_width', 'rrate', 'rotx', 'roty', 'rotz' ];
+  const str = [ 'color', 'wcolors', 'stroke' ];
+  const txt = (id) => document.querySelector('#' + id).value;
+  const val = (id) => parseFloat(txt(id));
 
-let mult = ([x,y,z], mat) => mat.map( ([a,b,c]) => a*x + b*y + c*z );
+  config = {}
 
-let rx = (th) => (v) => mult(v, [ [ 1, 0, 0 ], [ 0, cos(th), -sin(th) ], [ 0, sin(th), cos(th) ] ])
-let ry = (th) => (v) => mult(v, [ [ cos(th), 0, sin(th) ], [ 0, 1, 0 ], [ -sin(th), 0, cos(th) ] ])
-let rz = (th) => (v) => mult(v, [ [ cos(th), -sin(th), 0 ], [ sin(th), cos(th), 0 ], [ 0, 0, 1 ] ])
+  num.forEach(n => config[n] = val(n));
+  str.forEach(s => config[s] = txt(s));
 
-let x = (i,j) => r * sin(inc(i)) * cos(azi(j));
-let y = (i,j) => r * sin(inc(i)) * sin(azi(j));
-let z = (i,j) => r * cos(inc(i));
-
-let rot = [ 05, 33, 32 ];
+  return config;
+}
 
 let draw = () => {
   let svg = document.querySelector("#bal");
+  let config = readConfig();
   while( svg.hasChildNodes() ) svg.removeChild(svg.lastChild);
-  drawwheel(svg);
-  drawsphere(svg);
+  drawwheel(svg, config);
+  drawsphere(svg, config);
 }
 
-let spokes = 12;
-let wheel_radius = 3000;
-let wcx = 300, wcy = 300;
+window.onload = () => makeColorMatrix();
+
+
+// draw a wheel
+
+const wheel_radius = 3000;
 
 let wio = 0;
-let wx = (i) => wheel_radius * cos(2*PI*i/spokes+wio) + wcx;
-let wy = (i) => wheel_radius * sin(2*PI*i/spokes+wio) + wcy;
 
-let wheel_colors = [ [ 2, 'red' ], [ 3, 'green' ] ];
+const drawwheel = (svg, {wcx, wcy, wvel, wspokes, wcolors}) => {
+  const wx = (i) => wheel_radius * cos(2*PI*i/wspokes+wio) + wcx;
+  const wy = (i) => wheel_radius * sin(2*PI*i/wspokes+wio) + wcy;
 
-let drawwheel = (svg) => {
-  wio = isNaN(wio) ? 0 : wio + radians(val('wvel'));
-  wcx = min(1200, max(-400, val('wcx')));
-  wcy = min(1200, max(-400, val('wcy')));
-  wheel_colors = txt('wcolors').split(/\s+/).map(x => { s = x.split(/\,/); s[0] = parseInt(s[0]); return s;});
-  spokes = val('wspokes');
+  let wheel_colors;
+
+  wio = isNaN(wio) ? 0 : wio + radians(wvel);
+
+  wcx = min(1200, max(-400, wcx));
+  wcy = min(1200, max(-400, wcy));
+  wheel_colors = wcolors.split(/\s+/).map(x => { s = x.split(/\,/); s[0] = parseInt(s[0]); return s;});
 
   let spoke_size = wheel_colors.reduce( (sum, [width, col]) => sum + width, 0);
-  times(spokes)( i => {
+  times(wspokes)( i => {
     let w = 0;
     wheel_colors.forEach( ([width, col]) => {
       svg.appendChild(
@@ -101,18 +96,30 @@ let drawwheel = (svg) => {
   
 }
 
-let point = (i,j) => [ x(i,j), y(i,j), z(i,j) ];
 
-let drawsphere = (svg) => {
-  io = isNaN(io) ? 0 : io + radians(val('vel'));
-  lat_lines = val('lat_lines');
-  lon_lines = val('lon_lines');
-  stroke = txt('stroke');
-  stroke_width = val('stroke_width');
-  replacement_rate = val('rrate');
-  let rotx = val('rotx');
-  let roty = val('roty');
-  let rotz = val('rotz');
+// draw a sphere
+
+let io = 0;
+
+let drawsphere = (svg, {vel, lat_lines, lon_lines, stroke, stroke_width, rotx, roty, rotz}) => {
+  io = isNaN(io) ? 0 : io + radians(vel);
+
+  const r = 300;
+  const cx = 400, cy = 400;
+  const inc = (i) => (PI * i / lat_lines) % PI;
+  const azi = (j) => (io + 2 * PI * j / lon_lines) % (2 * PI);
+
+  const mult = ([x,y,z], mat) => mat.map( ([a,b,c]) => a*x + b*y + c*z );
+
+  const rx = (th) => (v) => mult(v, [ [ 1, 0, 0 ], [ 0, cos(th), -sin(th) ], [ 0, sin(th), cos(th) ] ])
+  const ry = (th) => (v) => mult(v, [ [ cos(th), 0, sin(th) ], [ 0, 1, 0 ], [ -sin(th), 0, cos(th) ] ])
+  const rz = (th) => (v) => mult(v, [ [ cos(th), -sin(th), 0 ], [ sin(th), cos(th), 0 ], [ 0, 0, 1 ] ])
+
+  const x = (i,j) => r * sin(inc(i)) * cos(azi(j));
+  const y = (i,j) => r * sin(inc(i)) * sin(azi(j));
+  const z = (i,j) => r * cos(inc(i));
+  const point = (i,j) => [ x(i,j), y(i,j), z(i,j) ];
+
 
   times(lat_lines)( i => times(lon_lines)( j => {
     // draw a polygon with corners at lat, lat+1, lon, lon+1
@@ -127,7 +134,9 @@ let drawsphere = (svg) => {
       point(i,k+0.5)
     ].map(
         p => rz(radians(rotx))(ry(radians(roty))(rx(radians(rotz))(p)))
-    ).filter(
+    );
+    
+    points = points.filter(
     	p => p[2] > 0
     );
 
@@ -135,7 +144,7 @@ let drawsphere = (svg) => {
     if (points.length > 1) {
       svg.appendChild(
         path(
-          color(i,j),
+          getColor(i,j),
           stroke,
           stroke_width,
           ...points.map(([x,y,z]) => [ x + cx, y + cy ])
