5125aa7287dbb6401920b61dac3a0937cf789ea0
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2020-07-31 18:21:42 -0700

    don't redraw nodes unless necessary

--- dscobal.js
+++ dscobal.js
@@ -1,4 +1,4 @@
-let {cos,sin,PI,random,floor,min,max} = Math;
+let {sqrt,cos,sin,PI,random,floor,min,max} = Math;
 
 const createElement = (type, atts) => {
   let shape = document.createElementNS("http://www.w3.org/2000/svg", type); 
@@ -10,8 +10,8 @@ const createElement = (type, atts) => {
 let stroke = 'darkgoldenrod'
 let stroke_width = 6;
 
-let path = (fill, stroke, stroke_width, ...points) => createElement('path',
-  { fill, stroke, 'stroke-width': `${stroke_width}px`, d: `M ${points.map(([x,y])=>`${x} ${y}`).join(' ')} Z` }
+let path = (fill, stroke, stroke_width) => createElement('path',
+  { fill, stroke, 'stroke-width': `${stroke_width}px`}
 );
 
 let text = (txt, x, y, fill, stroke, stroke_width, transform) => {
@@ -124,6 +124,11 @@ const updateConfig = () => {
   config.colors = config.color.split(/\s+/);
   config.spoke_size = config.wheel_colors.reduce( (sum, [width, col]) => sum + width, 0);
 
+  let svg = document.querySelector("#bal");
+  while( svg.hasChildNodes() ) svg.removeChild(svg.lastChild);
+
+  makeWheelElements(svg);
+  makeSphereElements(svg);
 }
 
 const readConfig = () => config;
@@ -131,8 +136,8 @@ const readConfig = () => config;
 const draw = () => {
   let svg = document.querySelector("#bal");
   let config = readConfig();
-  while( svg.hasChildNodes() ) svg.removeChild(svg.lastChild);
-  drawtext(svg, config);
+  drawwheel(config);
+  drawsphere(config);
 }
 
 window.onload = () => {
@@ -149,8 +154,6 @@ window.onload = () => {
     if (c == "default") option.selected = true;
     config.add(option);
   });
-
-  makeColorMatrix();
 }
 
 // draw orbiting text
@@ -179,7 +182,26 @@ const wheel_radius = 3000;
 
 let wio = 0;
 
-const drawwheel = (svg, {wcx, wcy, wvel, wspokes, spoke_size, wheel_colors}) => {
+let wheel_elements = [];
+
+const makeWheelElements = (svg) => {
+  console.log("Making wheel_elements");
+  let {wcx, wcy, wvel, wspokes, spoke_size, wheel_colors} = readConfig();
+
+  wheel_elements = times(wspokes)( i => times(wheel_colors.length)( j => {
+    let node = wheel_elements[i] && wheel_elements[i][j] ? wheel_elements[i][j] : {};
+
+    node.width = wheel_colors[j][0];
+    node.color = wheel_colors[j][1];
+    node.element = path(node.color, 'none', 0);
+
+    svg.appendChild(node.element);
+
+    return node;
+  }));
+}
+
+const drawwheel = ({wcx, wcy, wvel, wspokes, spoke_size, wheel_colors}) => {
   const wx = (i) => wheel_radius * cos(2*PI*i/wspokes+wio) + wcx;
   const wy = (i) => wheel_radius * sin(2*PI*i/wspokes+wio) + wcy;
 
@@ -187,49 +209,99 @@ const drawwheel = (svg, {wcx, wcy, wvel, wspokes, spoke_size, wheel_colors}) => 
 
   times(wspokes)( i => {
     let w = 0;
-    wheel_colors.forEach( ([width, col]) => {
-      svg.appendChild(
-        path(
-          col,
-          'none',
-          0,
-          [ wcx, wcy ],
-          [ wx(i+(w/spoke_size)), wy(i+(w/spoke_size)) ],
-          [ wx(i+(w+width)/spoke_size), wy(i+(w+width)/spoke_size) ]
-        )
-      );
-      w += width;
+
+    times(wheel_colors.length)( j => {
+      if (!wheel_elements[i]) return;
+
+      let node = wheel_elements[i][j];
+
+      if (!node) return;
+
+      let points = [
+            [ wcx, wcy ],
+            [ wx(i+(w/spoke_size)), wy(i+(w/spoke_size)) ],
+            [ wx(i+(w+node.width)/spoke_size), wy(i+(w+node.width)/spoke_size) ]
+      ].map(([x,y])=>`${x} ${y}`).join(' ');
+
+      node.element.setAttribute("d", `M ${points} Z` );
+
+      w += node.width;
     })
   });
-  
-}
 
+}
 
 // draw a sphere
 
-let io = 0;
+let sphere_elements = [];
 
-let drawsphere = (svg, {vel, lat_lines, lon_lines, stroke, stroke_width, rotx, roty, rotz}) => {
-  io = isNaN(io) ? 0 : io + radians(vel);
+const getColor = (color, colors, rrate) => random() < rrate ? colors[floor(random() * colors.length)] : (color || 'red');
 
-  const r = 300;
-  const cx = 400, cy = 400;
-  const inc = (i) => (PI * i / lat_lines) % PI;
-  const azi = (j) => (io + 2 * PI * j / lon_lines) % (2 * PI);
-
-  const mult = ([x,y,z], mat) => mat.map( ([a,b,c]) => a*x + b*y + c*z );
-
-  const rx = (th) => (v) => mult(v, [ [ 1, 0, 0 ], [ 0, cos(th), -sin(th) ], [ 0, sin(th), cos(th) ] ])
-  const ry = (th) => (v) => mult(v, [ [ cos(th), 0, sin(th) ], [ 0, 1, 0 ], [ -sin(th), 0, cos(th) ] ])
-  const rz = (th) => (v) => mult(v, [ [ cos(th), -sin(th), 0 ], [ sin(th), cos(th), 0 ], [ 0, 0, 1 ] ])
-
-  const x = (i,j) => r * sin(inc(i)) * cos(azi(j));
-  const y = (i,j) => r * sin(inc(i)) * sin(azi(j));
-  const z = (i,j) => r * cos(inc(i));
-  const point = (i,j) => [ x(i,j), y(i,j), z(i,j) ];
-
+const updateSphereColors = () => {
+  let {colors, lat_lines, lon_lines, rrate} = readConfig();
 
   times(lat_lines)( i => times(lon_lines)( j => {
+    // SphereElements was set, so we can assume the node exists
+    if (!sphere_elements[i]) return;
+
+    let node = sphere_elements[i][j];
+
+    if (!node) return;
+
+    sphere_elements[i][j].color = getColor(node.color, colors, rrate);
+  }))
+}
+
+const makeSphereElements = (svg) => {
+  console.log("Making sphere_elements");
+  let {colors, stroke, stroke_width, lat_lines, lon_lines, rrate} = readConfig();
+
+  sphere_elements = times(lat_lines)( i => times(lon_lines)( j => {
+    let node = sphere_elements[i] && sphere_elements[i][j] ? sphere_elements[i][j] : {}
+
+    // we may use the old color to "blur" transitions between dscobals
+    node.color = getColor(node.color, colors, rrate);
+
+    node.i = i;
+    node.j = j;
+
+    // while we preserve color above, the element is replaced
+    node.element = path(node.color, stroke, stroke_width);
+    svg.appendChild(node.element);
+
+    return node;
+  }))
+}
+
+let io = 0;
+
+const mult = ([x,y,z], mat) => mat.map( ([a,b,c]) => a*x + b*y + c*z );
+
+const rx = (th) => (v) => mult(v, [ [ 1, 0, 0 ], [ 0, cos(th), -sin(th) ], [ 0, sin(th), cos(th) ] ])
+const ry = (th) => (v) => mult(v, [ [ cos(th), 0, sin(th) ], [ 0, 1, 0 ], [ -sin(th), 0, cos(th) ] ])
+const rz = (th) => (v) => mult(v, [ [ cos(th), -sin(th), 0 ], [ sin(th), cos(th), 0 ], [ 0, 0, 1 ] ])
+
+const drawsphere = ({vel, lat_lines, lon_lines, rotx, roty, rotz}) => {
+  io = isNaN(io) ? 0 : io + radians(vel);
+
+  times(lat_lines)( i => times(lon_lines)( j => {
+    if (!sphere_elements[i]) return;
+
+    let node = sphere_elements[i][j];
+
+    if (!node) return;
+
+    const r = 300;
+    const cx = 400, cy = 400;
+    const inc = (i) => (PI * i / lat_lines) % PI;
+    const azi = (j) => (io + 2 * PI * j / lon_lines) % (2 * PI);
+
+
+    const x = (i,j) => r * sin(inc(i)) * cos(azi(j));
+    const y = (i,j) => r * sin(inc(i)) * sin(azi(j));
+    const z = (i,j) => r * cos(inc(i));
+    const point = (i,j) => [ x(i,j), y(i,j), z(i,j) ];
+
     // draw a polygon with corners at lat, lat+1, lon, lon+1
     let k = i % 2 == 0 ? j : j + 0.5;
     let m = (i == lat_lines-1) ? (i + 0.99) : (i + 1);
@@ -241,27 +313,23 @@ let drawsphere = (svg, {vel, lat_lines, lon_lines, stroke, stroke_width, rotx, r
       point(i,k+1),
       point(i,k+0.5)
     ].map(
-        p => rz(radians(rotx))(ry(radians(roty))(rx(radians(rotz))(p)))
-    );
-    
-    points = points.filter(
-    	p => p[2] > 0
+        p => rz(radians(rotz))(ry(radians(roty))(rx(radians(rotx))(p)))
     );
 
+    points = points.filter(p => p[2] > 0);
+
     // don't draw if z < 0 for all points
-    if (points.length > 1) {
-      svg.appendChild(
-        path(
-          getColor(i,j),
-          stroke,
-          stroke_width,
-          ...points.map(([x,y,z]) => [ x + cx, y + cy ])
-        )
-      );
-      }
+    if (points.length > 0) {
+      points = points.map(([x,y])=>`${x+cx} ${y+cy}`).join(' ');
+      node.element.setAttribute("d", `M ${points} Z` );
+    } else {
+      node.element.setAttribute("d", '');
+    }
+
+    node.element.setAttribute("fill", node.color);
   }))
 
 }
 
 setInterval(draw, 50);
-setInterval(makeColorMatrix, 500);
+setInterval(updateSphereColors, 500);
